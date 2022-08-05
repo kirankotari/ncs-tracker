53ef1da64bea762e76c6e423199c4422aa85e884
Kiran Kumar Kotari <kirankotari@live.com>
2022-08-05 11:59:47 -0400

    updated tracker class

--- ncs_tracker/tracker.py
+++ ncs_tracker/tracker.py
@@ -1,35 +1,42 @@
-from collections import defaultdict
+from time import time
 from logging import INFO, DEBUG
+from collections import defaultdict
 from .log import Logger
 
 
+# TODO: need to record sequence of methods called on tracker objects
+
 class Tracker:
-    tracker = defaultdict()
-    result = defaultdict()
+    tracker = defaultdict(list)
+    results = defaultdict()
 
     def update(self, method, tt):
-        # TODO: need to add/update tracker
-        pass
+        self.tracker[method].append(tt)
+
     def report(self):
-        # TODO: generate result
-        pass
+        """
+        This method will update the results in following format
+        method: (times-called, total-time-taken, list-of-taken-times)
+        """
+        for method, times in self.tracker.items():
+            self.results[method] = (len(self.tracker[method]), sum(self.tracker[method]), self.tracker[method])
 
 
-class ClassWrapper(Tracker):
-    tracker = {} # method: [operaton-time, ]
-    tracker_result = {} # method: (no of times called, total time, [operaton-time, ])
+class Proxy(Tracker):
     def __init__(self, obj, log):
         super().__init__()
         self.name = f"{obj.__class__.__name__} class"
-
-        self.obj = obj
-        self.log = log
+        self.log, self.obj = log, obj
         self.log.debug(f"{self.name} wrapped")
 
     def __getattr__(self, method):
         def mwrap(*args, **kwargs):
             self.log.debug(f"{self.name} calling `{method}` with params {args}, {kwargs}")
-            return getattr(self.obj, method)(*args, **kwargs)
+            st = time()
+            res = getattr(self.obj, method)(*args, **kwargs)
+            self.update(method, time() - st)
+            return res
+
         self.log.debug(f"{self.name} checking for `{method}`")
         if hasattr(self.obj, method):
             return mwrap
@@ -38,6 +45,7 @@ class ClassWrapper(Tracker):
             msg = f"{self.name}, no such field/method found"
             self.log.error(msg)
             raise AttributeError(msg)
+
 
 def get_logger(verbose):
     print("setting logger")
@@ -48,29 +56,42 @@ def get_logger(verbose):
     return log
 
 
-def track(*targs, timeit=False, verbose=False, before=None, after=None):
+def proxyWrapper(self, arg):
+    log = get_logger()
+    try:
+        if arg in self.__dir__():
+            exec("self.%s = Proxy(self.%s, log)" %(arg, arg))
+        else:
+            exec("%s = Proxy(%s, log)" %(arg, arg))
+    except Exception:
+        raise AttributeError('no variable `%s` found' %(arg))
+
+
+def proxyUnWrapper(self, arg):
+    log = get_logger()
+    try:
+        if arg in self.__dir__():
+            exec("self.%s = self.%s.obj" %(arg, arg))
+        else:
+            exec("%s = %s.obj" %(arg, arg))
+    except Exception:
+        raise AttributeError('no variable `%s` found' %(arg))
+
+
+def track(*targs, timeit=False, verbose=False): # before=None, after=None
     def wrapper(fun):
-        def wrapfun(fun_self, *args, **kwargs):
+        def wrapfun(self, *args, **kwargs):
             log = get_logger(verbose)
-            sequence_recorder = [] # TODO: add seq recorder
             for arg in targs:
-                """
-                TODO:
-                need to check the arg is part of fun_self class
-                need to check the arg is an argument to fun_self
-                """
-                exec('fun_self.%s' %(arg))
-                exec("fun_self.%s = ClassWrapper(fun_self.%s, log)" %(arg, arg))
+                proxyWrapper(self, arg)
                 log.debug(f"wrapped {arg}")
-            if before:
-                log.info(before)
-            # TODO: method time start
-            res = fun(fun_self, *args, **kwargs)
-            # TODO: method time end
-            if after:
-                log.info(after)
+
+            st = time()
+            res = fun(self, *args, **kwargs)
+            tt = time() - st
+
             for arg in targs:
-                exec("fun_self.%s = fun_self.%s.obj" %(arg, arg))
+                proxyUnWrapper(self, arg)
                 log.debug(f"un-wrapped {arg}")
             return res
         return wrapfun
